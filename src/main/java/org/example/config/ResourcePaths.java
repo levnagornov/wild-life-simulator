08ddda5cd4a1c62f52cb517813ce36870aa4543f
Lev Nagornov <levnagornov@yahoo.com>
2024-07-06 08:22:04 +0200

    feat: add resource paths

---
+++ src/main/java/org/example/config/ResourcePaths.java
@@ -0,0 +1,12 @@
+package org.example.config;
+
+import java.io.File;
+
+public class ResourcePaths {
+    public static final File DIETS_FILE = new File("./src/main/resources/diets.yml");
+    public static final File EMOJIS_FILE = new File("./src/main/resources/emojis.yml");
+    public static final File CHARACTERISTICS_FILE = new File("./src/main/resources/characteristics.yml");
+    public static final File SIMULATION_CONFIG_FILE = new File("./src/main/resources/simulation.yml");
+    public static final File TERMINATION_CONFIG_FILE = new File("./src/main/resources/termination.yml");
+    public static final File VIEW_CONFIG_FILE = new File("./src/main/resources/view.yml");
+}
