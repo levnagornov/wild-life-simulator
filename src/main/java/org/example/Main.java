f6648d172f66c199e7d41d223ad245a21c0386ac
Lev Nagornov <levnagornov@yahoo.com>
2024-07-08 20:51:05 +0200

    chore: add docs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,19 @@ import org.example.config.DependencyContainer;
 
 import java.util.concurrent.*;
 
+/**
+ * Main class to start and manage the simulation of a virtual ecosystem.
+ * It initializes necessary dependencies using a DependencyContainer, executes
+ * the simulation and plant regrowing tasks concurrently, and ensures proper
+ * termination and cleanup after completion or in case of exceptions.
+ */
 public class Main {
+
+    /**
+     * Main method to start the simulation.
+     *
+     * @param args Command line arguments (not used).
+     */
     public static void main(String[] args) {
         System.out.println("Simulation started.");
 
