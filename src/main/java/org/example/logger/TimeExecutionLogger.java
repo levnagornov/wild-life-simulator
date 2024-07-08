9538b211e7ab9ba316115c92d1af5813364818fd
Lev Nagornov <levnagornov@yahoo.com>
2024-07-08 20:24:33 +0200

    chore: add docs

--- src/main/java/org/example/logger/TimeExecutionLogger.java
+++ src/main/java/org/example/logger/TimeExecutionLogger.java
@@ -2,13 +2,28 @@ package org.example.logger;
 
 import org.slf4j.Logger;
 
+/**
+ * The {@code TimeExecutionLogger} class provides functionality to log the execution time of a task.
+ * It utilizes an {@code org.slf4j.Logger} to log the start, end, and duration of the task execution.
+ */
 public class TimeExecutionLogger {
     private final Logger logger;
 
+    /**
+     * Constructs a new {@code TimeExecutionLogger} with the specified logger.
+     *
+     * @param logger the logger to use for logging execution times
+     */
     public TimeExecutionLogger(Logger logger) {
         this.logger = logger;
     }
 
+    /**
+     * Logs the execution time of the specified task. The task's name and duration are logged.
+     *
+     * @param taskName the name of the task to be logged
+     * @param action   the task to be executed and timed
+     */
     public void logExecutionTime(String taskName, Runnable action) {
         var startTime = System.currentTimeMillis();
         logger.info("=== {} started ===", taskName);
